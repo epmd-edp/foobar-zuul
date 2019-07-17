1020f88a813d26566f44c73395ac9c2ae941fd19
Pavlo_Yemelianov <pavlo_yemelianov@epam.com>
2019-07-17 14:33:24 +0300

    Add client side load balancing
    Disable security
    Update Spring Boot version
    
    Change-Id: I0c8e7a0bfc4523cc38ffd66fcfd5157aa51a3d88

--- src/main/java/com/epam/edp/ZuulApplication.java
+++ src/main/java/com/epam/edp/ZuulApplication.java
@@ -1,7 +1,7 @@
 package com.epam.edp;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
@@ -9,6 +9,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ZuulApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+        SpringApplication.run(ZuulApplication.class, args);
     }
 }
